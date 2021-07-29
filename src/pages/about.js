09eae2ee9f83090f3991c19e2b0cc4e477b6db0c
Rayhan Rusyd <antianobl@gmail.com>
2021-07-29 08:14:34 +0700

    First commit of Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi there, I'm just started learning about React</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
